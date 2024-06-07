91dfd0556481cffb6a1ce50e2856fe202b689db7
Guilherme Fontes <guilherme.fontes98@outlook.com>
2024-06-07 16:04:20 +0100

    test

--- delegateMate/src/App.jsx
+++ delegateMate/src/App.jsx
@@ -9,11 +9,19 @@ import PersonalDetailsPage from "./pages/PersonalDetailsPage";
 import RequestPage from "./pages/RequestPage";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/companies" component={CompanyList} />
+        <Route path="/requests" component={RequestList} />
+        <Route path="/personal-details" component={PersonalDetails} />
+      </Switch>
+    </Router>
+  );
 }
-
 export default App;
-
 
 // jonny wuz here
 // renan wuz also here
+//guilherme wuz here as well
