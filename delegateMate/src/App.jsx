1e2c0a6873a45012b82658de8b3d9018c1c5578e
Renan <renan.castro@dashlane.com>
2024-06-07 11:54:54 +0100

    first commit, added vite, axios and router dom

--- delegateMate/src/App.jsx
+++ delegateMate/src/App.jsx
@@ -1,8 +1,15 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import About from './pages/About'
+import CompanyPage from './pages/CompanyPage'
+import HomePage from './pages/HomePage'
+import NotFound from './pages/NotFound'
+import PersonalDetailsPage from './pages/PersonalDetailsPage'
+import RequestPage from './pages/RequestPage'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
---
+++ delegateMate/src/pages/PersonalDetailsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PersonalDetailsPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PersonalDetailsPage
