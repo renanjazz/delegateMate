4893a1e420f940414cb6981136c2a7d4a050a84d
Ana Rosa <guilherme.fontes98@outlook.com>
2024-06-10 16:36:20 +0100

    changes

--- delegateMate/src/components/NavBar.jsx
+++ delegateMate/src/components/NavBar.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const NavBar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div></div>;
+};
 
-export default NavBar
+export default NavBar;
