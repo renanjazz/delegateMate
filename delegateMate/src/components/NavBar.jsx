9fad1df3a3cdcdd7da1f28b15e67b2475d368330
Jonathan Hingorani <jonathanhingorani@gmail.com>
2024-06-10 17:39:59 +0200

    radical

--- delegateMate/src/components/NavBar.jsx
+++ delegateMate/src/components/NavBar.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import logo from '../assets/delegateMateBlack.png'
+import { Link, useNavigate } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className='navbar'>
+      <img
+      src={logo}
+      alt='logo'
+      style={{height: "60px"}}
+      className='logo'
+      />
+      <Link to="/">
+        <button className='navbar-button'>About</button>
+      </Link>
+      <Link to="/terms">
+        <button className='navbar-button'>Terms</button>
+      </Link>
+      <Link to="/open-request">
+        <button className='navbar-button'>Account</button>
+      </Link>
+    </nav>
   )
 }
 
 export default NavBar
+
+
+// comment here
