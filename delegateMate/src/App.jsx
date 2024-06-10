28c7f6d4148fe975a9227e65ba65a7e0a3278ed0
Jonathan Hingorani <jonathanhingorani@gmail.com>
2024-06-10 11:33:52 +0200

    no tomato soup

--- delegateMate/src/App.jsx
+++ delegateMate/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import About from "./pages/About";
 import CompanyPage from "./pages/CompanyPage";
@@ -10,18 +10,17 @@ import RequestPage from "./pages/RequestPage";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
+    <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/companies" component={CompanyPage} />
         <Route path="/requests" component={RequestPage} />
         <Route path="/personal-details" component={PersonalDetailsPage} />
         <Route path="/login" component={LoginPage} />
         <Route path="/about" component={About} />
-        <Route component={NotFound} />
-      </Switch>
-    </Router>
+        <Route path="*" component={NotFound} />
+    </Routes>
   );
-}
+} 
 
 export default App;
+
