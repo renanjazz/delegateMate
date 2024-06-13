063899b705e8b8aaf823f165dc2398ffe6c75793
Guilherme Fontes <guilherme.fontes98@outlook.com>
2024-06-13 10:40:14 +0100

    guilherme update

--- delegateMate/src/pages/LoginPage.jsx
+++ delegateMate/src/pages/LoginPage.jsx
@@ -21,7 +21,9 @@ const LoginPage = ({ setCurrentUser }) => {
   const location = useLocation();
 
   // Retrieve the selected company from state or localStorage
-  const selectedCompany = location.state?.selectedCompany || JSON.parse(localStorage.getItem('selectedCompany'));
+  const selectedCompany =
+    location.state?.selectedCompany ||
+    JSON.parse(localStorage.getItem("selectedCompany"));
 
   useEffect(() => {
     if (!selectedCompany) {
@@ -34,7 +36,9 @@ const LoginPage = ({ setCurrentUser }) => {
     event.preventDefault();
     try {
       const { data } = await axios.get(`${API_URL}/users`);
-      const foundUser = data.find((oneUser) => oneUser.username.toLowerCase() === username.toLowerCase());
+      const foundUser = data.find(
+        (oneUser) => oneUser.username.toLowerCase() === username.toLowerCase()
+      );
       if (!foundUser) {
         setError("Invalid credentials");
       } else {
@@ -75,36 +79,6 @@ const LoginPage = ({ setCurrentUser }) => {
   return (
     <>
       <div>
-<<<<<<< HEAD
-        <h1>Login</h1>
-        {selectedCompany && (
-          <div>
-            <h3>Selected Company:</h3>
-            <p>{selectedCompany.title}</p>
-            <p>{selectedCompany.description}</p>
-          </div>
-        )}
-        <form onSubmit={handleLogin}>
-          <label>
-            Username:
-            <input
-              type="text"
-              value={username}
-              onChange={(event) => setUsername(event.target.value)}
-              placeholder="Username"
-            />
-          </label>
-          <label>
-            Password:
-            <input
-              type="password"
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-              placeholder="Password"
-            />
-            <button className="enter-button">Enter</button>
-          </label>
-=======
         <h2>Login</h2>
         <form className="login-form-and-enter">
           <div className="account-details-row">
@@ -131,72 +105,11 @@ const LoginPage = ({ setCurrentUser }) => {
               />
             </label>
           </div>
->>>>>>> 43f91c2f4d45e8d72d73f109afd2a42a198af21b
         </form>
 
         {error && (
           <p style={{ color: "red" }}>Invalid credentials, please try again</p>
         )}
-<<<<<<< HEAD
-        <h2>Don't have an account yet?</h2>
-        <form onSubmit={handleAddLogin}>
-          <label>
-            Name:
-            <input
-              type="text"
-              value={name}
-              onChange={(event) => setName(event.target.value)}
-              placeholder="Name"
-            />
-          </label>
-          <label>
-            Surname:
-            <input
-              type="text"
-              value={surName}
-              onChange={(event) => setSurname(event.target.value)}
-              placeholder="Surname"
-            />
-          </label>
-          <label>
-            Username:
-            <input
-              type="text"
-              value={createUsername}
-              onChange={(event) => setCreateUsername(event.target.value)}
-              placeholder="Username"
-            />
-          </label>
-          <label>
-            Password:
-            <input
-              type="password"
-              value={createPassword}
-              onChange={(event) => setCreatePassword(event.target.value)}
-              placeholder="Password"
-            />
-          </label>
-          <label>
-            Email:
-            <input
-              type="text"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-              placeholder="Email"
-            />
-          </label>
-          <label>
-            Telephone:
-            <input
-              type="text"
-              value={telephone}
-              onChange={(event) => setTelephone(event.target.value)}
-              placeholder="Telephone"
-            />
-          </label>
-          <label>
-            Address:
-=======
         <div className="enter-button-container">
           <button onClick={handleLogin} className="proceed-button">
             Enter
@@ -278,7 +191,6 @@ const LoginPage = ({ setCurrentUser }) => {
             </label>
           </div>
           <label className="label-login">
->>>>>>> 43f91c2f4d45e8d72d73f109afd2a42a198af21b
             <input
               className="forms-address"
               type="text"
@@ -287,30 +199,6 @@ const LoginPage = ({ setCurrentUser }) => {
               placeholder="Address"
             />
           </label>
-<<<<<<< HEAD
-          <label>
-            City:
-            <input
-              type="text"
-              value={city}
-              onChange={(event) => setCity(event.target.value)}
-              placeholder="City"
-            />
-          </label>
-          <label>
-            Postcode:
-            <input
-              type="text"
-              value={postcode}
-              onChange={(event) => setPostcode(event.target.value)}
-              placeholder="Postcode"
-            />
-          </label>
-          <Link to="/open-request">
-            <button className="back-button">Back</button>
-          </Link>
-          <button className="submit-button">Submit</button>
-=======
           <div className="account-details-row">
             <label className="label-login">
               <input
@@ -342,7 +230,6 @@ const LoginPage = ({ setCurrentUser }) => {
 
             <button className="proceed-button">Submit</button>
           </div>
->>>>>>> 43f91c2f4d45e8d72d73f109afd2a42a198af21b
         </form>
       </div>
     </>
